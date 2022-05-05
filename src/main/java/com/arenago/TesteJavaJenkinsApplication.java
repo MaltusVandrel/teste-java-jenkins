5175943d9f12a8a6cef74308647961f7577acdae
Matheus G. Bento <matheus.bento@eqsengenharia.com.br>
2022-05-05 17:22:18 -0300

    go

---
+++ src/main/java/com/arenago/TesteJavaJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.arenago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJavaJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJavaJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arenago/TesteJavaJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arenago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJavaJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
